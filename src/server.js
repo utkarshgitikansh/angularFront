85c2b720f09e746af6860084db9f6386f072c0f5
utkarshgitikansh <utkarshgitikansh@gmail.com>
2018-10-08 15:34:07 +0530

    55th commit

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ app.use(express.static('./dist/angular-front'));
 
 app.get('/*', (req,res)=>{
 
-    res.sendFile(path.join(__dirname,'./dist/angular-front/index.html'));
+    res.sendFile(path.join(__dirname,'/dist/angular-front/index.html'));
 });
 
 app.listen(process.env.PORT || 8080, ()=>{
