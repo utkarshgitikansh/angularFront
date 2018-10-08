798c1ce1374a83ef7b7abd4f8c1765101d198536
utkarshgitikansh <utkarshgitikansh@gmail.com>
2018-10-08 18:19:22 +0530

    79th commit

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ path = require('path');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static('./dist/angularFront'));
+app.use(express.static(__dirname + '/dist/angularFront'));
 // Start the app by listening on the default
 // Heroku port
 
 app.get('/*', (req,res)=>{
 
-    res.sendFile(path.join(__dirname,'./dist/angularFront/index.html'));
+    res.sendFile(path.join(__dirname + './dist/angularFront/index.html'));
 });
 
 app.listen(process.env.PORT || 8080)    
