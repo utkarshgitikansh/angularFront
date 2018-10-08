7a753579190db8e88165668b8e226b5c4aa33f83
utkarshgitikansh <utkarshgitikansh@gmail.com>
2018-10-08 18:01:38 +0530

    78th commit

--- server.js
+++ server.js
@@ -12,4 +12,4 @@ app.get('/*', (req,res)=>{
     res.sendFile(path.join(__dirname,'./dist/angularFront/index.html'));
 });
 
-app.listen(process.env.PORT || 8080)
+app.listen(process.env.PORT || 8080)    
